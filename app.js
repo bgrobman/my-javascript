e6ad2f3615f96c3571327d72e4c140c9167eb8da
ranan grobman <binyamin.grobman@gmail.com>
2018-11-12 15:30:18 +0200

    crating the app

---
+++ app.js
@@ -0,0 +1,13 @@
+const location = document.getElementById('location');
+const size = document.getElementById('size');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click',(event)=>{
+if(size.target.value === 8 Hours){
+
+}
+if(location.target.value === Israel){
+
+
+}
+});
