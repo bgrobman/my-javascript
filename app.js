fb1b89f83336179f0c9a49ae53b3bb04b1f9b631
ranan grobman <binyamin.grobman@gmail.com>
2018-11-18 16:49:17 +0200

    corected header request

--- app.js
+++ app.js
@@ -8,35 +8,45 @@ const number = document.querySelector('#number');
 const code = document.querySelector('#project');
 // let user = uName + email + number + size + code + loc ;
 let numberOfUsers = 0;
+let check = false;
+
+function err(obb){
+alert('you did not fill in the required info');
+}
+
 
 
 
 submit.addEventListener('click',(event)=>{
   numberOfUsers += 1;
 
+  if(uName.value === '' || number.value === '' || email.value === '' || loc.value === ''){
+  check = false;
+  err();
+}else{
+check = true;
+}
+
+
+if(check === true){
   var xhr = new XMLHttpRequest();
   var url = 'https://api.airtable.com/v0/appGMs3wYCKeVkQnl/Table%201?api_key=keyZ3s4zJ9nbnOFdZ';
+  var data = {
+  "fields": {
+   "Name":uName.value,
+   "Email address":email.value,
+   "Number": number.value,
+   "Time  of work requested":size.value,
+   "Location of user":loc.value,
+   "Type Of Work": code.value
+  }
+  };
 
   xhr.open('POST',url);
-
-
  xhr.setRequestHeader('Content-Type','application/json');
-
-var data = {
-"fields": {
- "Name":uName.value,
- "Email address":email.value,
- "Number": number.value,
- "Time  of work requested":size.value,
- "Location of user":loc.value
-}
-};
-
-
 xhr.send(JSON.stringify(data));
 
-
-
+}
 });
 
 
