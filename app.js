bf14dc1f4907d22ea2c60d214084c802ac9ce58c
ranan grobman <binyamin.grobman@gmail.com>
2018-11-19 15:17:55 +0200

    some changes

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const submit = document.getElementById('submit');
+const inspect = document.getElementById('check');
 const size = document.getElementById('size');
 const loc = document.querySelector('#place');
 const email = document.querySelector('#email');
 const uName = document.querySelector('#name');
 const number = document.querySelector('#number');
 const code = document.querySelector('#project');
- let user =  [size ,loc,email,uName,number,code];
-let numberOfUsers = 0;
-let check = true;
+const user = [uName,email,number];
 
+let check = false;
+let empty = '';
+
+function test(val){
+  for(let i = 0; i < val.length ; i += 1){
+    if(val[i].value === ''){
+    empty += '" ' + val[i].name + '",';
+    }else{
+   check = true;
+ }
+}
+}
+
+
+//sends data to server
 
 
 
 submit.addEventListener('click',(event)=>{
-  numberOfUsers += 1;
+  test(user);
+if(check === false){
+  alert('you did not fill in the required info for the '  + empty + ' input(s).');
+}
 
-  for(let i = 0; i < user.length ; i += 1){
-    if(user[i].value === ''){
-    alert('you did not fill in the required info for the ' +user[i].name + ' input.' );
-   check = false;
-    }
-  }
-
-if(check === true ){
+  if(check === true){
   var xhr = new XMLHttpRequest();
   var url = 'https://api.airtable.com/v0/appGMs3wYCKeVkQnl/Table%201?api_key=keyZ3s4zJ9nbnOFdZ';
   var data = {
