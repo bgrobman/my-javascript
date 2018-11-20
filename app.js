57f50cece48a75937d35c62a80aae362b8893001
ranan grobman <binyamin.grobman@gmail.com>
2018-11-20 16:37:39 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -6,46 +6,88 @@ const email = document.querySelector('#email');
 const uName = document.querySelector('#name');
 const number = document.querySelector('#number');
 const code = document.querySelector('#project');
-const user = [uName,email,number];
+const user = [uName,email,number,loc];
 
 let check = false;
-let empty = '';
+let valid = false;
+var url = 'https://api.airtable.com/v0/appGMs3wYCKeVkQnl/Table%201?api_key=keyZ3s4zJ9nbnOFdZ';
 
+
+// test if inputs are empty
 function test(val){
+let empty = '';
   for(let i = 0; i < val.length ; i += 1){
     if(val[i].value === ''){
-    empty += '" ' + val[i].name + '",';
-    }else{
-   check = true;
+   empty += '" ' + val[i].name + '",';
+   check = false;
  }
 }
+
+  if(empty !== ''){
+    alert('You did not fill in the info for the ' + empty + ' input(s).' );
+} else{
+  check = true;
 }
+
+}
+
+//email validation
+function ValidateEmail(mail)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+  {
+  }else{
+    alert("You have entered an invalid email address!");
+    check = false;
+
+  }
+}
+//check if input is number
+function isNumber(val){
+ if(!isNaN(val.value)){
+alert('you need to write letters for the ' + val.name + ' input.')
+check = false;
+}
+}
+
+//check if data already excists in database
+function validate(field){
+  let xhr1 = new XMLHttpRequest();
+  xhr1.open('GET',url);
+  xhr1.onreadystatechange = function(){
+    if(xhr1.readyState === 4){
+
+
+      }
+    }
+  xhr1.send();
+}
+
+
+
 
 
 //sends data to server
-
-
-
 submit.addEventListener('click',(event)=>{
   test(user);
-if(check === false){
-  alert('you did not fill in the required info for the '  + empty + ' input(s).');
+if(check === true){
+ValidateEmail(email);
+  isNumber(uName);
 }
+
+var data = {
+"fields": {
+ "Name":uName.value,
+ "Email address":email.value,
+ "Number": number.value,
+ "Time  of work requested":size.value,
+ "Location of user":loc.value,
+ "Type Of Work": code.value
+}
+};
 
   if(check === true){
   var xhr = new XMLHttpRequest();
-  var url = 'https://api.airtable.com/v0/appGMs3wYCKeVkQnl/Table%201?api_key=keyZ3s4zJ9nbnOFdZ';
-  var data = {
-  "fields": {
-   "Name":uName.value,
-   "Email address":email.value,
-   "Number": number.value,
-   "Time  of work requested":size.value,
-   "Location of user":loc.value,
-   "Type Of Work": code.value
-  }
-  };
-
   xhr.open('POST',url);
  xhr.setRequestHeader('Content-Type','application/json');
 xhr.send(JSON.stringify(data));
